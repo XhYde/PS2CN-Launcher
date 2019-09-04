1f137c2f1c7537d914d4cdc5a3677bf99fb0eb0a
Xhyde <hyde_chen@hotmail.com>
2019-09-04 20:39:51 +0800

    Live upd 20190828

--- PS2CN Launcher/SteamHelp.vb
+++ PS2CN Launcher/SteamHelp.vb
@@ -3,15 +3,15 @@
     Private Sub SteamHelp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         '窗体在主界面左侧显示，且不超出屏幕左上
-        Me.Left = IIf( _
-            PS2CN_Launcher.LauncherForm1.Left - 639 > 0, _
-            PS2CN_Launcher.LauncherForm1.Left - 639, _
-            0 _
+        Me.Left = IIf(
+            LauncherForm1.Left - 639 > 0,
+            LauncherForm1.Left - 639,
+            0
             )
-        Me.Top = IIf( _
-            PS2CN_Launcher.LauncherForm1.Bottom - 591 > 0, _
-            PS2CN_Launcher.LauncherForm1.Bottom - 591, _
-            0 _
+        Me.Top = IIf(
+            LauncherForm1.Bottom - 591 > 0,
+            LauncherForm1.Bottom - 591,
+            0
             )
     End Sub
 End Class
