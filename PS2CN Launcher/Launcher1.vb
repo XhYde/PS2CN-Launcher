7108a528e61d2837ed34b19b6f25746dc6d3fb5e
Xhyde <hyde_chen@hotmail.com>
2020-04-24 17:36:11 +0800

    Live upd 20200415

--- PS2CN Launcher/Launcher1.vb
+++ PS2CN Launcher/Launcher1.vb
@@ -3,7 +3,7 @@ Imports System.Net.NetworkInformation      'process类，用于获得系统进�
 
 Public Class LauncherForm1
 
-    Const Version As UInt32 = 202003271      '软件版本号，8位日期 + 1位当日版本。每次【更新】记得【修改】！
+    Const Version As UInt32 = 202004241      '软件版本号，8位日期 + 1位当日版本。每次【更新】记得【修改】！
 
     '每次更新后，github先提交【更改】，再【同步】到服务器
 
